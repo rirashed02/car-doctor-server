6dd5d64e544d478e341ccf15f2bd8a831a66b839
rirashed02 <rirashed02@gmail.com>
2023-09-12 15:56:03 -0700

    currently there is no work on this server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,25 +21,119 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+  }
+
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db('carsDoctor').collection('services')
+    const bookingCollection = client.db('carsDoctor').collection('bookings')
+
+    // jwt use
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      console.log(token)
+
+      // that's mean ekhane object hisabe pathabe  property hocche token value token, variable same nam hole ek nam dilei cholbe
+      res.send({ token })
+    })
+
+    // services
+    app.get('/services', async (req, res) => {
+      const cursor = servicesCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { _id: 1, title: 1, service_id: 1, price: 1, img: 1 }
+      }
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result)
+    })
+
+    // bookings
+    app.get('/bookings', verifyJWT, async (req, res) => {
+      const decoded = req.decoded
+      console.log('came back after verify', decoded)
+
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: 'forbidden access' })
+      }
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      // console.log(booking)
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result)
+    })
+
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateBooking = req.body;
+      // console.log(updateBooking)
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status
+        }
+      }
+      const result = await bookingCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('car doctor server is running')
+  res.send('car doctor server is running')
 });
 
 app.listen(port, (req, res) => {
-    console.log(`car doctor server is running on PORT: ${port}`)
+  console.log(`car doctor server is running on PORT: ${port}`)
 })
